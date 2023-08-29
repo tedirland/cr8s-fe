241663fb3638c4cabe94a774969ce33115d0eded
tedirland <irlandth@gmail.com>
2023-08-29 13:25:34 -0500

    Added Yew as a dependency and created a Yew FC

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use yew::prelude::*;
+
+// this is the root component of the application
+// two options - functional components and object component
+// FCs are a lot easier to understand, maintain and read
+#[function_component(App)]
+fn app() -> Html {
+    html!{
+        <>
+        <p>{"Hello world"}</p>
+
+        </>
+
+    }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new()
+    .render();
 }
